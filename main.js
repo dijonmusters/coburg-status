f7c1aa4e38e18c79cddd7f09ab82b384f811fb56
Jon Meyers <jon@Jons-iMac.lan>
2017-04-28 22:53:03 +1000

    Load HTML express

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
-// var express = require('express');
-// var app = express();
-// var router = express.Router();
-//
-// app.get('/', function (req, res) {
-//   res.sendFile(__dirname + '/index.html');
-// });
-//
-// app.get('/other', function (req, res) {
-//   res.sendFile(__dirname + '/other.html');
-// });
-//
-// app.listen(5000, function () {
-//   console.log('Example app listening on port 5000!');
-// });
-var http = require('http');
-http.createServer(function (req, res) {
-  // res.writeHead(200, {"Content-Type": "text/plain"});
-  // res.end("Hello node friend\n");
+var express = require('express');
+var app = express();
+var router = express.Router();
+
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
-}).listen(process.env.PORT);
+});
+
+app.get('/other', function (req, res) {
+  res.sendFile(__dirname + '/other.html');
+});
+
+app.listen(process.env.PORT);
+// var http = require('http');
+// http.createServer(function (req, res) {
+//   // res.writeHead(200, {"Content-Type": "text/plain"});
+//   // res.end("Hello node friend\n");
+//   res.sendFile(__dirname + '/index.html');
+// }).listen(process.env.PORT);
