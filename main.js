e30ac803b17d4826e628d9dec38f1d1c09edf185
Jon Meyers <jon@Jons-iMac.lan>
2017-04-28 22:45:26 +1000

    Simplify app

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-var express = require('express');
-var app = express();
-var router = express.Router();
-
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/other', function (req, res) {
-  res.sendFile(__dirname + '/other.html');
-});
-
-app.listen(5000, function () {
-  console.log('Example app listening on port 5000!');
-});
+// var express = require('express');
+// var app = express();
+// var router = express.Router();
+//
+// app.get('/', function (req, res) {
+//   res.sendFile(__dirname + '/index.html');
+// });
+//
+// app.get('/other', function (req, res) {
+//   res.sendFile(__dirname + '/other.html');
+// });
+//
+// app.listen(5000, function () {
+//   console.log('Example app listening on port 5000!');
+// });
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.end("Hello node friend\n");
+}).listen(process.env.PORT);
