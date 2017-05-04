1474ea1b2a4cebfb6de05a68b65f2b455ef48914
Jon Meyers <jon@Jons-MacBook-Pro.local>
2017-05-04 22:45:12 +1000

    ADD ping all test function

--- main.js
+++ main.js
@@ -10,6 +10,16 @@ app.get('/other', function (req, res) {
   res.sendFile(__dirname + '/other.html');
 });
 
+app.get('/email', function(req, res) {
+  response.send(process.env.EMAIL);
+});
+
+var ping_all = function(req, res) {
+  res.send('hello from node!!');
+}
+
+app.post('/ping_all', ping_all);
+
 app.listen(process.env.PORT);
 // var http = require('http');
 // http.createServer(function (req, res) {
