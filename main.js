c90027bac4b34b70f841939a6f80177e54fdeada
Jon Meyers <jon@Jons-iMac.local>
2017-05-05 14:46:32 +1000

    TEST response

--- main.js
+++ main.js
@@ -18,22 +18,31 @@ app.get('/email', function(req, res) {
 
 var ping_all = function(req, res) {
   var hosts = [
-    'https://student22.coburg.vic.edu.au',
-    'https://coburg-vic.compass.education/'
+    'https://www.student20.coburg.vic.edu.au',
+    'https://www.student22.coburg.vic.edu.au/wiki',
+    '10.147.252.32'
   ];
+  var cfg = {
+    timeout: 1000,
+    // WARNING: -i 2 may not work in other platform like window
+    extra: ["-i 2"],
+  };
   var msg = 'PINGED';
   hosts.forEach(function (host) {
     ping.sys.probe(host, function(isAlive) {
-      msg += 'testing';
-    });
-  }).then(function(msg) {
-    res.send(msg);
+      var msg = isAlive ? 'host ' + host + ' is alive' : 'host ' + host + ' is dead';
+      console.log(msg);
+    }, {extra: ["-i 2"], timeout: false});
   });
+  res.send(msg);
 }
 
 app.post('/ping_all', ping_all);
 
-app.listen(process.env.PORT);
+// app.listen(process.env.PORT);
+app.listen(5000, function() {
+  console.log('listening...');
+});
 // var http = require('http');
 // http.createServer(function (req, res) {
 //   // res.writeHead(200, {"Content-Type": "text/plain"});
