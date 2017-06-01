9b587d06858a3967412a72dff813de391649c607
LCL Admin <lcladmin@c02kq033dnmn.chs.vic.edu.au>
2017-06-01 16:57:40 +1000

    FIX external GET requests

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
-const ping = require('ping');
 const https = require('https');
 
 app.get('/', function (req, res) {
@@ -43,27 +42,6 @@ var ping_all = function(req, res) {
       status: ''
     }
   ];
-  // var i = 0;
-  // hosts.forEach(function(host) {
-  //   var options = {
-  //     hostname :  host.address,
-  //     port : 443,
-  //     rejectUnauthorized: false,
-  //     path: '/',
-  //     headers: {'Cache-Control':'no-cache', accept: '*/*'}
-  //   };
-  //   https.get(options, function(res) {
-  //     i++;
-  //     if (i === hosts.length) {
-  //       res.send(hosts);
-  //     }
-  //     host.status = res.statusCode;
-  //     console.log(res.statusCode);
-  //   }).on('error', (e) => {
-  //     console.error('error: ' + e);
-  //     host.status = 400;
-  //   });
-  // });
   var i = 0;
   hosts.forEach(function (host) {
     var options = {
@@ -73,17 +51,32 @@ var ping_all = function(req, res) {
       path: '/',
       headers: {'Cache-Control':'no-cache', accept: '*/*'}
     };
-    var req = https.get(options, (resp) => {
-      host.status = resp.statusCode;
+    var req = https.get(options, function(resp) {
+      host.status = 'online';
       i++;
       if (i === hosts.length) {
         res.send(hosts);
       }
-    }).on('error', (e) => {
-      console.error('error: ' + e);
-      host.status = 400;
+      resp.on('data', function() {} );
+      console.log(resp.statusCode);
     });
-
+    req.on('socket', function(socket) {
+      socket.setTimeout(1000);
+      socket.on('timeout', function() {
+          req.abort();
+      });
+    });
+    req.on('error', function(e) {
+      if (e.code == 'EHOSTDOWN' || e.code == 'ETIMEDOUT')
+        console.log('DOWN');
+      if (e.code == 'ECONNRESET')
+        console.log('DROPPED');
+      host.status = 'offline';
+      i++;
+      if (i === hosts.length) {
+        res.send(hosts);
+      }
+    });
   });
   // setTimeout(ping_all, 15000);
 }
@@ -97,14 +90,3 @@ if (process.env.PORT) {
     console.log('listening...');
   });
 }
-
-
-// app.listen(5000, function() {
-//   console.log('listening...');
-// });
-// var http = require('http');
-// http.createServer(function (req, res) {
-//   // res.writeHead(200, {"Content-Type": "text/plain"});
-//   // res.end("Hello node friend\n");
-//   res.sendFile(__dirname + '/index.html');
-// }).listen(process.env.PORT);
